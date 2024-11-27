16e4aa39ef9664b0b5b0b7aed7c232a5499370f5
BastianLedezmaElgueta17 <bastianledezmaelgueta@gmail.com>
2024-11-27 20:39:28 -0300

    console

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),icon({iconDir: "src/assets/icons",})]
+  integrations: [tailwind(),icon()]
 });
